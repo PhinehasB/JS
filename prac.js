79b8c8d2f962d84240ba7f39711bc93dee1e36da
Phinehas Borketey <phinehasborketey42@gmail.com>
2023-05-26 16:08:22 +0000

    version 1

---
+++ prac.js
@@ -0,0 +1,16 @@
+console.log(19);
+console.log(1);
+console.log("My name is Phinehas Borketey Bortey");
+console.log(Math.floor(200.40));
+console.log('Hello'.length);
+console.log(Math.floor(Math.random()*100));
+var me="Phinehas"
+    console.log(me);
+    me= "Borketey";
+    console.log(me);
+    let myName;
+    myName=500;
+    console.log(myName);
+    const mine="Borteye";
+    console.log(mine);
+    
